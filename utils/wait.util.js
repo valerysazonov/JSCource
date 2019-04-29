afe41e05afb993239704278fac9c49a0a2812f2f
Valery Sazonov <v.sazonov@a1qa.com>
2019-04-29 16:21:46 +0300

    HW2 - done

--- utils/wait.util.js
+++ utils/wait.util.js
@@ -2,31 +2,38 @@ const logger = require('./log.util');
 
 const doWait = (action, interval, expectedValue) => {
     return new Promise((resolve, reject) => {
-        if (action() === expectedValue) {
+        const actionResult = action();
+        if (actionResult === expectedValue) {
             setTimeout(() => resolve(), interval);
         }
-        setTimeout(() => reject(), interval);
-        return action();
-    })
-}
+        setTimeout(() => reject(actionResult), interval);
+    });
+};
 
+const retrier = (action, interval, expectedValue, maxCount, count) => {
+    count++;
+    logger.info(`[${count}] Wait for ${expectedValue}`);
+    return doWait(action, interval, expectedValue).then(() => {
+        logger.info('Was able to reach expected condition!');
+        return true;
+    }, (actionResult) => {
+        if (count >= maxCount) {
+            logger.warning('Was not able to reach expected condition!');
+            logger.warning('Result: ' + actionResult);
+            return false;
+        } else {
+            return retrier(action, interval, expectedValue, maxCount, count);
+        }
+    });
+};
 
 class Wait {
-    forTrue(action, maxCount, interval, count = 0) {
-        count++
-        logger.info(`[${count}] Wait for true`)
-        return doWait(action, interval, true).then(() => {
-            logger.info('Was able to reach expected condition!');
-            return true;
-        }, () => {
-            if (count >= maxCount) {
-                logger.warning('Was not able to reach expected condition!');
-                logger.warning('Result: ' + action())
-                return false;
-            } else {
-                return this.forTrue(action, maxCount, interval, count)
-            }
-        })
+    forTrue(action, maxCount, interval) {
+        return retrier(action, interval, true, maxCount, 0);
+    }
+
+    forFalse(action, maxCount, interval) {
+        return retrier(action, interval, false, maxCount, 0);
     }
 }
 
